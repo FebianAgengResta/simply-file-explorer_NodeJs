991475050260fa00e09db5bcc81f31c70125ef6b
bongs <restaageng@gmail.com>
2023-12-17 23:03:51 +0700

    first commit

---
+++ playground/humansize.js
@@ -0,0 +1,11 @@
+console.log(Math.log10(1000));
+console.log(Math.log10(10000));
+
+
+const filesize = 5758575; 
+const units = "BKMGT";
+
+const index = Math.floor(Math.log10(filesize)/3);
+const filesizeHuman = (filesize/Math.pow(1000,index)).toFixed(1);
+
+console.log(`${filesizeHuman}${units[index]}`);
